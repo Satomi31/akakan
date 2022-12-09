42330b54c271bc172bdd1e80ee51ec8081083b48
Satomi31 <aquos.01310@gmail.com>
2022-12-09 07:18:51 +0000

    ログアウトボタン修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Authority;
 
 class UsersController extends Controller
 {
   public function index(){
     $users = User::all();
+    // $authorities = Authority::all();
+    // $users = User::with('authorities')->get();
     return view('index', ['users' => $users]);
   }
     //
